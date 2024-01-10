b61862aa25fcc6fd1012fe657647d016d394ab5a
Rune stat <runejeppesen@hotmail.com>
2024-01-10 19:20:10 +0100

    fdsa

--- child/vite.configORG.ts
+++ child/vite.configORG.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vitest/config";
 import preact from "@preact/preset-vite";
-import { VitePWA } from "vite-plugin-pwa";
 //import compress from "vite-plugin-compress";
+import { VitePWA } from "vite-plugin-pwa";
 import { registerRoute } from "workbox-routing";
 export default defineConfig({
   plugins: [
