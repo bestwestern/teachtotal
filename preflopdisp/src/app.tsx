b7520f97422cdb707dbc65f05ec62771639b3232
John Doe <runejeppesen@hotmail.com>
2024-01-12 19:19:03 +0100

    preflop

---
+++ preflopdisp/src/app.tsx
@@ -0,0 +1,58 @@
+import { useState } from "preact/hooks";
+import { pl } from "./pl";
+console.log({ pl });
+export function App() {
+  const [programme, set_programme] = useState(pl[0].title);
+  const [exercise, set_exercise] = useState(pl[0].exercises[0].title);
+  const changeProgramme = (newProgramme) => {
+    set_programme(newProgramme);
+    set_exercise(
+      pl.find((prog) => prog.title === newProgramme).exercises[0].title
+    );
+  };
+  const selected_programme = pl.find((prog) => prog.title === programme);
+  const possible_blinds = [];
+  const selected_exercise = selected_programme.exercises.find(
+    (ex) => ex.title === exercise
+  );
+  console.log({ selected_exercise });
+  return (
+    <>
+      <div class="btn-group" role="group" aria-label="Basic outlined example">
+        {pl.map(({ title }) => (
+          <button
+            type="button"
+            onclick={(e) => changeProgramme(title)}
+            class={
+              programme === title
+                ? "btn btn-primary"
+                : "btn btn-outline-primary"
+            }
+          >
+            {title}
+          </button>
+        ))}
+      </div>
+      <br />
+      <div class="btn-group" role="group" aria-label="Basic outlined example">
+        {selected_programme.exercises.map(({ title }) => (
+          <button
+            type="button"
+            onclick={(e) => set_exercise(title)}
+            class={
+              exercise === title ? "btn btn-primary" : "btn btn-outline-primary"
+            }
+          >
+            {title}
+          </button>
+        ))}
+      </div>
+      <br />
+      <img
+        src={selected_exercise.pictureId + ".webp"}
+        class="img-fluid"
+        alt="..."
+      ></img>
+    </>
+  );
+}
